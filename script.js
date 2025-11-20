1b8deea2a76e0a58d91fa3aec17bff7642c897db
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-20 18:51:24 +0530

    Normal Function And Anonymous function

---
+++ script.js
@@ -0,0 +1,16 @@
+//Function Types
+
+//1.Normal function
+function add(a,b){
+    return a+b
+}
+console.log(add(2,3))
+
+
+//2.Anonymous function
+
+const subtraction = function(a,b){
+    return a-b
+}
+console.log(subtraction(10,6))
+
