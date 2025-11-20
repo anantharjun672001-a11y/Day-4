dda8451c491403c3efed1549b11c3236305ef47c
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-20 19:29:43 +0530

    IIFE Function updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Function Types
 
-//1.Normal function
+/* //1.Normal function
 function add(a,b){
     return a+b
 }
@@ -14,3 +14,18 @@ const subtraction = function(a,b){
 }
 console.log(subtraction(10,6))
 
+//2.a Anonymous function type 2
+
+setTimeout(function(){
+    console.log("This runs after 5 seconds");
+    
+},2000)
+
+//3.Arrow function
+const Multiply =(a,b) =>a*b
+console.log(Multiply(3,5)) */
+
+//4.IIFE Function
+(function (){
+    console.log("This Function runs immediately");
+})();
